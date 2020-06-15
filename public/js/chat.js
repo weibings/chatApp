dce185b5110f3494d8adfc211fba6af8d5b8fe01
bin wei <binwei1988@gmail.com>
2020-06-15 17:34:57 -0400

    added awknowledgements

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,4 +1,11 @@
 const socket = io();
+
+//Elements
+const $messageForm = document.querySelector("#messageForm");
+const $messageFormInput = document.querySelector("input");
+const $messageFormButton = document.querySelector("button");
+const $locationButton = document.querySelector("#send-location");
+
 socket.on("message", (welcome)=>{
     console.log(welcome);
 })
@@ -7,18 +14,32 @@ socket.on("send", (data)=>{
     console.log(data);
 })
 
-// document.querySelector("#formSample").addEventListener("submit", (e) =>{
-//     e.preventDefault();
-//     const message = document.querySelector("input").value;
+$messageForm.addEventListener("submit", (e) =>{
+    e.preventDefault();
+    $messageFormButton.setAttribute("disabled", "disabled");
+    const message = document.querySelector("input").value;
 
-//     socket.emit("sendMessage", message);
-// })
+    socket.emit("sendMessage", message, (error) => {
+        $messageFormButton.removeAttribute("disabled");
+        $messageFormInput.value = "";
+        $messageFormInput.focus();
 
-document.querySelector("#send-location").addEventListener('click', () =>{
+        if(error){
+            return console.log(error);
+        }
+        console.log("Message delivered");
+    });
+})
+
+$locationButton.addEventListener('click', () =>{
+    $locationButton.setAttribute("disabled", "disabled");
     if(!navigator.geolocation){
         return alert('Geolocation is not supported by your browser')
     }
     navigator.geolocation.getCurrentPosition((position) =>{
-        socket.emit("sendLocation", {lat: position.coords.latitude, long: position.coords.longitude})
+        socket.emit("sendLocation", {lat: position.coords.latitude, long: position.coords.longitude}, ()=>{
+            $locationButton.removeAttribute("disabled");
+            console.log("Location shared!");
+        })
     })
 })
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const path = require("path");
 const http = require("http");
 const express = require("express");
 const socketio = require("socket.io");
-
+const Filter = require("bad-words");
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
@@ -19,13 +19,19 @@ io.on('connection', (socket) => {
     // socket.emit("message", {welcome: "hello"});
     socket.broadcast.emit("message", "A new user has joined!");
 
-    socket.on("sendMessage", (message) =>{
+    socket.on("sendMessage", (message, callback) =>{
         // console.log(message);
+        const filter = new Filter();
+        if(filter.isProfane(message)){
+            return callback("Profanity is not allowed");
+        }
         io.emit("send", `${message} received`);
+        callback();
     })
 
-    socket.on("sendLocation", (position) =>{
-        io.emit("message",`https://google.com/maps?q=${position.lat},${position.long}`)
+    socket.on("sendLocation", (position, callback) =>{
+        io.emit("message",`https://google.com/maps?q=${position.lat},${position.long}`);
+        callback();
     })
 
     socket.on("disconnect", () => {
